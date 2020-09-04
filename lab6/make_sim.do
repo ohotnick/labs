8d7044cd5775262dfd6ab26e6835cddabbd5f931
ohotnick <70637688+ohotnick@users.noreply.github.com>
2020-09-04 21:54:22 +0300

    Изменения версия 1
    Внес скрипт make_sim.do. Добавил регистр  logic  data_val_i_p. Форматирование кода. Изменен тестбэнч

---
+++ lab6/make_sim.do
@@ -0,0 +1,11 @@
+transcript on
+
+vlib work
+
+vlog -sv bit_population_counter.sv
+vlog -sv top_tb6.sv
+
+vsim -novopt top_tb6
+add log -r /*
+add wave -r *
+run 3000
--- lab6/top_tb6.sv
+++ lab6/top_tb6.sv
@@ -1,63 +1,87 @@
 module top_tb6;
+
 bit clk;
 bit reset;
  
+parameter WIDTH = 5; 
+
+logic [WIDTH-1:0]           data_i_t;                      //data in
+logic                       data_val_i_t;
+
+logic [($clog2(WIDTH)+1):0] data_o_t;			 //out
+logic                       data_val_o_t;	
+
 initial 
-	forever 
-		#100 clk=!clk;
-//#10000 clk=!clk;
-
-parameter WIDTH = 5;
-  		
-
-logic [WIDTH-1:0]data_i_t;     //вход данные /команда
-logic data_val_i_t;
-
-logic [($clog2(WIDTH)+1):0]data_o_t;						//вых
-logic data_val_o_t;	
-
-
-
-
+  forever 
+    #100 clk =! clk;
 
 initial
   begin
-    @(posedge clk)
-    begin
-    reset<=1'b1;
-    end
-    @(posedge clk)
-    reset<=1'b0;
+    @( posedge clk )
+      reset <= 1'b1;	  
+    @( posedge clk )
+      reset <= 1'b0;
   end
  
  
 initial
   begin
-   #300; 
-   @(posedge clk)
-   begin
-   data_i_t<=5'b00100;  
-   data_val_i_t<=1;
-	end   
-   @(posedge clk)   
-   @(posedge clk)
+    #300; 
+    @( posedge clk )
       begin
-   data_i_t<=5'b10101;  
-   data_val_i_t<=1;
-	end  
-   @(posedge clk)
-   @(posedge clk)
+        data_i_t     <= 5'b00100;  
+        data_val_i_t <= 1; 
+	  end   
+    @( posedge clk )   
       begin
-   data_i_t<=5'b00100;  
-   data_val_i_t<=0;
-	end  
-	   @(posedge clk)
-   @(posedge clk)
+	    $display( "input  1data: %b"  , data_i_t     );
+	    $display( "input  valid: %b"  , data_val_i_t );
+	  end
+    @( posedge clk )
       begin
-   data_i_t<=5'b11111;  
-   data_val_i_t<=1;
-	end  
-	
+        data_i_t     <= 5'b10101;  
+        data_val_i_t <= 1;   
+	  end  
+    @( posedge clk )
+      begin
+	    $display( "output  data:%d"    , data_o_t     );
+	    $display( "output valid: %b\n" , data_val_o_t );
+	  
+	    $display( "input  2data: %b"   , data_i_t     );
+	    $display( "input  valid: %d"   , data_val_i_t );
+	  end
+    @( posedge clk )
+      begin
+        data_i_t     <= 5'b00100;  
+        data_val_i_t <= 0;
+	  end  
+    @( posedge clk )
+      begin
+	    $display( "output  data:%d"    , data_o_t     );
+	    $display( "output valid: %b\n" , data_val_o_t );
+	  
+	    $display( "input  3data: %b"   , data_i_t     );
+	    $display( "input  valid: %d"   , data_val_i_t );
+	  end
+    @( posedge clk )
+      begin
+        data_i_t     <= 5'b11111;  
+        data_val_i_t <= 1;
+	  end  
+	@( posedge clk )
+	  begin
+	    $display( "output  data:%d"    , data_o_t     );
+	    $display( "output valid: %b\n" , data_val_o_t );
+	  
+	    $display( "input  4data: %b"   , data_i_t     );
+	    $display( "input  valid: %d"   , data_val_i_t );
+	  end	
+	@( posedge clk )
+	@( posedge clk )
+	  begin
+	    $display( "output  data:%d"    , data_o_t     );
+	    $display( "output valid: %b\n" , data_val_o_t );
+	  end
 
   end  
 
@@ -70,14 +94,14 @@ bit_population_counter DUT
 
 )*/
 (
-	.clk_i(clk),
-	.srst_i(reset),
+	.clk_i      ( clk          ),
+	.srst_i     ( reset        ),
 
-	.data_i(data_i_t),
-	.data_val_i(data_val_i_t),
+	.data_i     ( data_i_t     ),
+	.data_val_i ( data_val_i_t ),
 
-	.data_o(data_o_t),
-	.data_val_o(data_val_o_t)
+	.data_o     ( data_o_t     ),
+	.data_val_o ( data_val_o_t )
 );
   
 endmodule
