431037ca40d9514aba22a4717963cd5954d7513f
ohotnick <portnick@bk.ru>
2020-09-25 10:01:27 +0300

    1.1

--- lab7/debouncer.sv
+++ lab7/debouncer.sv
@@ -1,56 +1,71 @@
 module debouncer #(
-parameter CLK_FREQ_MHZ = 20,
-parameter GLITCH_TIME_NS = 50
-)
-(
-input clk_i,
+  parameter CLK_FREQ_MHZ = 20,
+  parameter GLITCH_TIME_NS =50
+)(
+  input clk_i,
 
-input key_i,
+  input key_i,
 
-output key_pressed_stb_o
+  output key_pressed_stb_o
 );
 
-parameter CLK_TIME = 1/CLK_FREQ_MHZ;		//50ns и меньше идет в 1 такт
-parameter WIDTH = CLK_TIME/GLITCH_TIME_NS;
+parameter CLK_TIME_NS = 1000/CLK_FREQ_MHZ;		
+parameter WIDTH    = GLITCH_TIME_NS/CLK_TIME_NS;
 
-logic key_pressed_stb_o_p;
-logic per;
-logic [($clog2(WIDTH)+1):0]data_o_p;		//колличество тактов
-logic key_i_p;
-
-
-always_ff @(posedge clk_i)		//по клоку---------------------
-begin
-
-key_i_p=key_i;
-
-if(key_i_p != 1)
-begin
-key_pressed_stb_o_p <= 0;
-per<=0;
-data_o_p<=0;
-key_pressed_stb_o_p<=0;
-end 
-
-if(key_i_p == 1 && per==0)
-begin
-data_o_p <= data_o_p +1;
-end 
-
-if(per==1)
-key_pressed_stb_o_p<=0;
-else
-begin
-if(data_o_p >=(WIDTH+1))
-begin
-key_pressed_stb_o_p<=1;
-per<=1;
-end
-end
+logic key_pressed_stb_o_tv;
+logic flag;
+logic [($clog2(WIDTH)+1):0]data_o_c;		//counter
 
 
-end   //always_ff
 
-assign key_pressed_stb_o = key_pressed_stb_o_p;
+
+always_ff @( posedge clk_i )		
+  begin
+    if( key_i != 1 || key_i== 1 )
+	  begin
+        if( key_i == 0 && flag == 0 )
+          begin
+            flag                 <=1;
+            data_o_c            <=0;
+            key_pressed_stb_o_tv <=0;
+          end 
+        if( key_i == 0 )
+          flag                 <=1;			  
+	  end
+	else
+	  data_o_c            <=0;  
+
+    if( flag== 1 && key_i == 1 )
+      begin
+        data_o_c <= data_o_c + 1;
+      end 
+	else
+      if ( data_o_c >= 1)
+	    data_o_c <= data_o_c + 1;
+	  
+    if( flag == 0 )
+      key_pressed_stb_o_tv<=0;
+    else
+      begin
+        if( data_o_c >= ( WIDTH ) )
+          begin
+            if( key_i == 1 )
+			  begin
+                key_pressed_stb_o_tv<=1;
+                flag<=0;
+			    data_o_c            <=0;
+			  end
+			else
+    		  begin
+                flag<=0;
+			    data_o_c            <=0;
+			  end
+          end
+      end
+  end  
+
+
+
+assign key_pressed_stb_o = key_pressed_stb_o_tv;
 
 endmodule
--- lab7/top_tb7.sv
+++ lab7/top_tb7.sv
@@ -2,62 +2,117 @@ module top_tb7;
 bit clk;
 //bit reset;
  
+parameter GLITCH_TIME_NS_T = 500;
+parameter CLK_FREQ_MHZ_T   = 20; 
+ 
 initial 
-	forever 
-		#100 clk=!clk;
-//#10000 clk=!clk;
-/*
-parameter CLK_FREQ_MHZ = 20;
-parameter GLITCH_TIME_NS = 50;
-parameter CLK_TIME = 1/CLK_FREQ_MHZ;		//50ns и меньше идет в 1 такт
-parameter WIDTH = CLK_TIME/GLITCH_TIME_NS;
-  		
-*/
-logic key_i_t;     //вход данные /команда
+  forever 
+    #(1000000/CLK_FREQ_MHZ_T/2) clk=!clk;
 
-logic key_pressed_stb_o_t;						//вых
+logic key_i_t;     //input button
+
+logic key_pressed_stb_o_t;					
+
+logic flag_b;
+int ttt;
+int   ttt1=0;
+int   ttt2=0;
 
 
 
+task send ( );
 
-
-/*
-initial
+  @( posedge clk )
+  
+  for (int k = 0; k < GLITCH_TIME_NS_T; k++)
   begin
-    @(posedge clk)
-    begin
-    reset<=1'b1;
-    end
-    @(posedge clk)
-    reset<=1'b0;
+    ttt = $urandom%20;
+    if( ttt > 14 )
+	  key_i_t = 1;
+	else
+      key_i_t = 0;
+    #1000;	    
+	  
+    $display( "send %d" , ttt );
   end
- */
+  key_i_t = 1;
+  for (int k = 0; k < GLITCH_TIME_NS_T/(1000/CLK_FREQ_MHZ_T); k++)
+  @( posedge clk );
+  key_i_t = 0;
+endtask
+
+
+
+task get ( );
+
+  forever
+    begin
+	 @( posedge clk )
+     if( key_i_t == 1 )
+	   begin
+	     ttt1 = $time;
+		 break;
+	   end
+    end
+  
+    forever
+    begin
+	 @( posedge clk )
+     if( key_pressed_stb_o_t == 1 )
+	   begin
+	     ttt2 = $time;
+		 break;
+	   end
+    end
+  $display( "ttt1 %d:   ttt2 %d rez %d " , ttt1, ttt2, ttt2-ttt1);	
+  
+endtask
+
+
+
+
+task compare ( int temp1, int temp2 );
+
+    if( ( ttt2 - ttt1 - 1000000/CLK_FREQ_MHZ_T ) == ( GLITCH_TIME_NS_T * 1000 ) )
+      $display( "Glitch time is done" );
+    else
+      $error("no match of results");
+ 
+endtask
+
+
+
 
 initial
   begin
-   #100; 
-   @(posedge clk)
-   key_i_t<=0;  
-      @(posedge clk)
-   key_i_t<=1;  
-   #400; 
-   @(posedge clk)
-   key_i_t<=0;  
-      @(posedge clk)
-   key_i_t<=1; 
-
+    #500 
+	flag_b = 0;
+    @( posedge clk )
+	key_i_t = 0;
+    @( posedge clk )
+    @( posedge clk )
+    @( posedge clk )	
 	
+	fork
+      begin
+        send(  );
+      end
+      begin
+        get(); 
+      end		  
+	join
+	  begin
+	    compare( ttt1, ttt2  );
+	    $display( "end ------- " );
+		$stop;
+      end 
+  end
 
-  end  
-
-
-  
-  
-debouncer DUT 
-/*#(
-	.WIDTH (5),
-
-)*/
+debouncer 
+#(
+  .CLK_FREQ_MHZ   ( CLK_FREQ_MHZ_T ),
+  .GLITCH_TIME_NS ( GLITCH_TIME_NS_T )
+)DUT 
 (
 	.clk_i(clk),
 
